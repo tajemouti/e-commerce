5976499f102a119a6e287074f41cac727993b3d3
tajemouti <tajemouti@gmail.com>
2024-05-31 15:26:38 +0100

    Add useState to control the underline in the navigation menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import cart from '../assets/cart_icon.png'
 import './navbar.css'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
         <div className="nav-logo">
@@ -11,12 +13,10 @@ const Navbar = () => {
             <p>Shopper</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop 
-              <hr />
-            </li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
