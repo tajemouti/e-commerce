daf0aefc8290c89425df19e499cbf427abcd9011
tajemouti <tajemouti@gmail.com>
2024-06-01 12:53:29 +0100

    Add category props

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory />} />
-          <Route path='/women' element={<ShopCategory />} />
-          <Route path='/kids' element={<ShopCategory />} />
+          <Route path='/men' element={<ShopCategory category='men' />} />
+          <Route path='/women' element={<ShopCategory category='women' />} />
+          <Route path='/kids' element={<ShopCategory category='kids' />} />
           <Route path='/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
           <Route path='/cart' element={<Cart />}/>
-          <Route path='/login' element={<LoginSignup />}/>
+          <Route path='/login' element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>
