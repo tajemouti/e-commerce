33a8e6eb0c9a584104b4d2dba93c3daa22bfc2be
tajemouti <tajemouti@gmail.com>
2024-06-01 12:15:08 +0100

    Define shop as the default route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Shop from './pages/Shop';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
