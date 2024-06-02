844093c157195d335223a41fe6d1796073019ec8
tajemouti <tajemouti@gmail.com>
2024-06-02 14:47:20 +0100

    Style left side of the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,26 +5,28 @@ import arrow_icon from '../assets/arrow.png'
 import hero_image from '../assets/hero_image.png'
 
 const Hero = () => {
-  return (
-    <div className='hero'>
-        <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div className="hand-icon">
-                <p>New</p>
-                <img src={hand_icon} alt="hand-icon" />
-                <p>Collections</p>
-                <p>for everyone</p>
+    return (
+        <div className='hero'>
+            <div className="hero-left">
+                <h2>NEW ARRIVALS ONLY</h2>
+                <div>
+                    <div className="hero-hand-icon">
+                        <p>New</p>
+                        <img src={hand_icon} alt="hand-icon" />
+                    </div>
+                    <p>collections</p>
+                    <p>for everyone</p>
+                </div>
+                <div className="hero-latest-btn">
+                    <div>Lastest Collection</div>
+                    <img src={arrow_icon} alt="arrow-icon" />
+                </div>
             </div>
-            <div className="hero-latest-btn">
-                <div>Lastest Collection</div>
-                <img src={arrow_icon} alt="arrow-icon" />
+            <div className="hero-right">
+                <img src={hero_image} alt="hero" />
             </div>
         </div>
-        <div className="hero-right">
-            <img src={hero_image} alt="hero" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
