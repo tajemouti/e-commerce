38344a8bfa34f0db8f39f873c8f2f148863696c3
tajemouti <tajemouti@gmail.com>
2024-06-02 15:27:56 +0100

    Layout the html structure for Popular component

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './popular.css'
 import data_product from '../assets/data'
+import Item from '../Item/Item'
 
 const Popular = () => {
   return (
     <div className='popular'>
-        Popular
+        <h1>POPULAR IN WOMEN</h1>
+        <hr />
+        <div className="popular-item">
+            {data_product.map((item) => {
+                return <Item />
+            })}
+        </div>
     </div>
   )
 }
