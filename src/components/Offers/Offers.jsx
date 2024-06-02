23f3658526ee3c1487e5ef410c5d5df718101ee4
tajemouti <tajemouti@gmail.com>
2024-06-02 16:04:36 +0100

    Define the structure for Offers component

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './offers.css'
+import exclusive_image from '../assets/exclusive_image.png'
 
 const Offers = () => {
   return (
     <div className='offers'>
-        Offers
+        <div className="offers-left">
+            <h1>Exclusive<br/>Offers For You</h1>
+            <p>ONLY THE BEST SELLERS PRODUCTS</p>
+            <button>Check Now</button>
+        </div>
+        <div className="offers-right">
+            <img src={exclusive_image} alt="exclusive" />
+        </div>
     </div>
   )
 }
