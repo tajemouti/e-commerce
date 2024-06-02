a623561bf172d62a4153ac2d17244bdf21913e2e
tajemouti <tajemouti@gmail.com>
2024-06-02 15:03:30 +0100

    Define the structure for the Item component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './item.css'
 
-const Item = () => {
+const Item = (props) => {
   return (
     <div className='item'>
-        Item
+        <img src={props.image} alt="item" />
+        <p>{props.nmae}</p>
+        <div className="item-prices">
+            <div className="item-price-new">
+                {props.new_price}
+            </div>
+            <div className="item-price-old">
+                {props.old_price}
+            </div>
+        </div>
     </div>
   )
 }
