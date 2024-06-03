03610e36cf9d695b7eb8b5042598f7de2ca2031f
tajemouti <tajemouti@gmail.com>
2024-06-03 21:55:46 +0100

    Defile html structure for NewsLetter

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -3,7 +3,14 @@ import './NewsLetter.css'
 
 const NewsLetter = () => {
   return (
-    <div className='newsletter'>NewsLetter</div>
+    <div className='newsletter'>
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Type your E-mail here' />
+            <button>Subscribe</button>
+        </div>
+    </div>
   )
 }
 
