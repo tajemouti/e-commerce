a920d3b488f4f7ae800bc39367cb812b17b012db
tajemouti <tajemouti@gmail.com>
2024-06-03 10:59:36 +0100

    Create NewCollections component

---
+++ src/components/NewCollections/NewCollections.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewCollections = () => {
+  return (
+    <div className='new_collections'>
+        NewCollections
+    </div>
+  )
+}
+
+export default NewCollections
