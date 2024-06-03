7ca1a31d5168f32215c8bbe89533fb3de63e4161
tajemouti <tajemouti@gmail.com>
2024-06-03 21:52:37 +0100

    Create NewsLetter component

---
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NewsLetter.css'
+
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>NewsLetter</div>
+  )
+}
+
+export default NewsLetter
