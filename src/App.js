2426e77a62e8c10c9c4e72115b1d35b666bd480a
tajemouti <tajemouti@gmail.com>
2024-06-06 13:38:15 +0100

    Define the props banner

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory category='men' />} />
-          <Route path='/women' element={<ShopCategory category='women' />} />
-          <Route path='/kids' element={<ShopCategory category='kids' />} />
+          <Route path='/men' element={<ShopCategory banner={men_banner} category='men' />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category='women' />} />
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category='kids' />} />
           <Route path='/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
