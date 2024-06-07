566c5dfda26850a5f7d26cd9faba88ae2b5d3cae
tajemouti <tajemouti@gmail.com>
2024-06-07 12:38:01 +0100

    Create Sign Upd section HTLM structure

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div classname="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='E-mail address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Log in here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
