ccf8df7239487d3cb5c83d5b779b024de2c4a1bf
tajemouti <tajemouti@gmail.com>
2024-06-08 15:39:00 +0100

    Create a new component Breadcrum

---
+++ src/components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './breadcrum.css'
+
+const Breadcrum = () => {
+  return (
+    <div className='breadcrum'>
+
+    </div>
+  )
+}
+
+export default Breadcrum
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
-import {ShopContext} from '../Context/ShopContext'
+import {ShopContext} from '../context/ShopContext'
 import { useParams } from 'react-router-dom'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
   const {productId} = useParams();
   const product = all_product.find((e) => e.id === Number(productId));
-  
+
   return (
     <div>
 
