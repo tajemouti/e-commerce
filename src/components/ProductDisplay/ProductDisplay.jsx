1ae69dcdb0429430af9ce72d5588543994b10591
tajemouti <tajemouti@gmail.com>
2024-06-09 17:36:59 +0100

    Create ProductDisplay component

---
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './product_display.css'
+
+const ProductDisplay = () => {
+  return (
+    <div className='product-display'>
+
+    </div>
+  )
+}
+
+export default ProductDisplay
