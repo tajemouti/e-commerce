f5ff9fab7bd4f3448d63b693c6fcd3e62625bc29
tajemouti <tajemouti@gmail.com>
2024-06-09 16:36:30 +0100

    Create HTML structure for Breadcrum component

--- src/components/Breadcrums/Breadcrum.jsx
+++ src/components/Breadcrums/Breadcrum.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './breadcrum.css'
+import arrow_icon from '../assets/breadcrum_arrow.png'
 
-const Breadcrum = () => {
+const Breadcrum = (props) => {
+  const {product} = props;
+
   return (
     <div className='breadcrum'>
-
+      HOME <img src={arrow_icon} alt="arrow" /> SHOP <img src={arrow_icon} alt="arrow" /> {product.category} <img src={arrow_icon} alt="arrow" /> {product.name}
     </div>
   )
 }
