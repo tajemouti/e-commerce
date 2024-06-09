a544baf55e544fd3d47123714a69ee11ab2183e0
tajemouti <tajemouti@gmail.com>
2024-06-09 20:14:54 +0100

    Update DisplayProduct.jsx

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -4,44 +4,45 @@ import star_icon from '../assets/star_icon.png'
 import star_dull_icon from '../assets/star_dull_icon.png'
 
 const ProductDisplay = (props) => {
-  const {product} = props;
+    const { product } = props;
 
-  return (
-    <div className='product-display'>
-        <div className="product-display-left">
-            <div className="product-display-img-list">
-                <img src={product.image} alt="product" />
-                <img src={product.image} alt="product" />
-                <img src={product.image} alt="product" />
-                <img src={product.image} alt="product" />
-            </div>
-            <div className="product-display-img">
-                <img className="product-display-main-img" src={product.image} alt="product" />
-            </div>
-        </div>
-        <div className="product-display-right">
-            <h1>{product.name}</h1>
-            <div className="product-display-right-start">
-                <img src={star_icon} alt="star" />
-                <img src={star_icon} alt="star" />
-                <img src={star_icon} alt="star" />
-                <img src={star_icon} alt="star" />
-                <img src={star_dull_icon} alt="star-dull" />
-                <p>(122)</p>
-            </div>
-            <div className="product-display-right-prices">
-                <div className="product-display-right-price-old">
-                    {product.old_price}
+    return (
+        <div className='product-display'>
+            <div className="product-display-left">
+                <div className="product-display-img-list">
+                    <img src={product.image} alt="product" />
+                    <img src={product.image} alt="product" />
+                    <img src={product.image} alt="product" />
+                    <img src={product.image} alt="product" />
                 </div>
-                <div className="product-display-right-price-new">
-                    {product.new_price}
+                <div className="product-display-img">
+                    <img className="product-display-main-img" src={product.image} alt="product" />
+                </div>
+            </div>
+            <div className="product-display-right">
+                <h1>{product.name}</h1>
+                <div className="product-display-right-star">
+                    <img src={star_icon} alt="star" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_icon} alt="star" />
+                    <img src={star_dull_icon} alt="star-dull" />
+                    <p>(122)</p>
+                </div>
+                <div className="product-display-right-prices">
+                    <div className="product-display-right-price-old">
+                        {product.old_price}
+                    </div>
+                    <div className="product-display-right-price-new">
+                        ${product.new_price}
+                    </div>
                 </div>
                 <div className="product-display-right-description">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam facere quo, iste asperiores harum maiores recusandae deserunt est natus culpa, voluptatum aliquid odit ipsa aspernatur excepturi nam minima voluptate pariatur.
                 </div>
                 <div className="product-display-right-size">
                     <h1>Select size</h1>
-                    <div className="product-display-right-size">
+                    <div className="product-display-right-sizes">
                         <div>S</div>
                         <div>M</div>
                         <div>L</div>
@@ -60,8 +61,7 @@ const ProductDisplay = (props) => {
                 </p>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductDisplay
