6f7702c98d185f4ac7047e9f678ed214ee856ede
tajemouti <tajemouti@gmail.com>
2024-06-09 18:01:15 +0100

    Define the HTML structure for ProdutDisplay component

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
 import './product_display.css'
+import star_icon from '../assets/star_icon.png'
+import star_dull_icon from '../assets/star_dull_icon.png'
 
-const ProductDisplay = () => {
+const ProductDisplay = (props) => {
+  const {product} = props;
+
   return (
     <div className='product-display'>
-
+        <div className="product-display-left">
+            <div className="product-display-img-list">
+                <img src={product.image} alt="product" />
+                <img src={product.image} alt="product" />
+                <img src={product.image} alt="product" />
+                <img src={product.image} alt="product" />
+            </div>
+            <div className="product-display-img">
+                <img className="product-display-main-img" src={product.image} alt="product" />
+            </div>
+        </div>
+        <div className="product-display-right">
+            <h1>{product.name}</h1>
+            <div className="product-display-right-start">
+                <img src={star_icon} alt="star" />
+                <img src={star_icon} alt="star" />
+                <img src={star_icon} alt="star" />
+                <img src={star_icon} alt="star" />
+                <img src={star_dull_icon} alt="star-dull" />
+                <p>(122)</p>
+            </div>
+            <div className="product-display-right-prices">
+                <div className="product-display-right-price-old">
+                    {product.old_price}
+                </div>
+                <div className="product-display-right-price-new">
+                    {product.new_price}
+                </div>
+                <div className="product-display-right-description">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam facere quo, iste asperiores harum maiores recusandae deserunt est natus culpa, voluptatum aliquid odit ipsa aspernatur excepturi nam minima voluptate pariatur.
+                </div>
+                <div className="product-display-right-size">
+                    <h1>Select size</h1>
+                    <div className="product-display-right-size">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                <p className="product-display-right-category">
+                    <span>Category :</span>
+                    Women, T-shirt, Crop Top
+                </p>
+                <p className="product-display-right-category">
+                    <span>Tags :</span>
+                    Modern, Latest
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
