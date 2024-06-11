ebd410ae961ce2cd4afdbac0819c46fa3c364447
tajemouti <tajemouti@gmail.com>
2024-06-11 23:22:00 +0100

    Mount DescriptionBox into the Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import {ShopContext} from '../context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../components/Breadcrums/Breadcrum';
 import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -13,6 +14,7 @@ const Product = () => {
     <div>
       <Breadcrum product={product}/>
       <ProductDisplay product={product}/>
+      <DescriptionBox />
     </div>
   )
 }
