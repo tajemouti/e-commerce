9d70009eaed1c27753fc0f5755cd6a531639e8aa
tajemouti <tajemouti@gmail.com>
2024-06-13 15:18:11 +0100

    Define HTML structure for RelatedProducts

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './related_products.css'
+import data_product from '../assets/data'
+import Item from '../Item/Item'
 
 const RelatedProducts = () => {
   return (
-    <div className='related-products'>RelatedProducts</div>
+    <div className='related-products'>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="related-products-item">
+            {data_product.map((item, i) => {
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
+    </div>
   )
 }
 
