dc57482bda33f55a127ba3c836a5bbf75547e63a
tajemouti <tajemouti@gmail.com>
2024-06-22 13:11:43 +0100

    Add HTML structure for CartItems down section

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -18,21 +18,50 @@ const CartItems = () => {
       </div>
       <hr />
       {all_product.map((e) => {
-        if (cartItems[e.id] > 0)
-        {
+        if (cartItems[e.id] > 0) {
           return <div>
-          <div className="cart-items-format cart-items-format-main">
-            <img src={e.image} alt="product" className='cart-icon-product-icon'/>
-            <p>{e.name}</p>
-            <p>${e.new_price}</p>
-            <button className='cart-items-quantity'>{cartItems[e.id]}</button>
-            <p>${e.new_price*cartItems[e.id]}</p>
-            <img src={remove_icon} className='cart-items-remove-icon' onClick={() => {removeFromCart(e.id)}} alt="remove-icon" />
+            <div className="cart-items-format cart-items-format-main">
+              <img src={e.image} alt="product" className='cart-icon-product-icon' />
+              <p>{e.name}</p>
+              <p>${e.new_price}</p>
+              <button className='cart-items-quantity'>{cartItems[e.id]}</button>
+              <p>${e.new_price * cartItems[e.id]}</p>
+              <img src={remove_icon} className='cart-items-remove-icon' onClick={() => { removeFromCart(e.id) }} alt="remove-icon" />
+            </div>
+            <hr />
           </div>
-          <hr />
-        </div>
         }
+        return null
       })}
+      <div className="cart-items-down">
+        <div className="cart-items-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cart-items-total-item">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cart-items-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cart-items-total-item">
+              <h3>Total</h3>
+              <h3>${0}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-items-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cart-items-promobox">
+            <input type="text" placeholder='Promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
